aa416cc0cc14d9b3a565487aa90a5423aed1711c
Kyaw Thet Paing <100781623+imkyawthetpaing@users.noreply.github.com>
2022-06-16 20:25:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 var True = "true";
 var False = "false";
-var X = 'x';
-var O = 'o';
+var MutiMode = "muti";
+var AiMode = "aimode"
+var X = 'X';
+var O = 'O';
 var Default = "none";
 var UserOne = "userone";
 var UserTwo = "usertwo";
 var AI = 'ai';
+var othername;
 
+var Mode;
 var WhoTurn = UserOne;
-var UserOneSymbol = X;
+var UserOneSymbol = O;
 var UserTwoSymbol = O;
-var AiSymbol = O;
+var AiSymbol = X;
+var playerScore = 0;
+var otherScore = 0;
 
 var Neumorphism = '#dde1e7';
 var White = "white";
 var Black = "black";
+
 var None = "none";
 var Block = "block";
 
@@ -28,7 +35,12 @@ var IndexMarginLeftAndRight = (BoardLenght - IndexLenght*3)/4;
 var IndexMarginTop = IndexMarginLeftAndRight/2;
 var IndexColor = Black;
 
+var chooseMode = document.getElementById('chooseMode');
+var chooseModeContainer = document.getElementById('chooseModeContainer');
+var chooseSymbolContainer = document.getElementById('chooseSymbolContainer');
+
 var gameMode = document.getElementById('gameMode');
+var otherName = document.getElementById('otherName');
 var board =       document.getElementById('board');
 
 var rowOne =       document.getElementById('row1');
@@ -64,12 +76,18 @@ var oEight =  document.getElementById('o8');
 var xNine =   document.getElementById('x9');
 var oNine =   document.getElementById('o9');
 
+var score = document.getElementById('score');
+
+var playerSymbol = document.getElementById('playerSymbol');
+var otherSymbol =   document.getElementById('otherSymbol');
+
 // official
 gameMode.style.height = 100 + 'vh';
 gameMode.style.backgroundColor = Black;
 gameMode.style.display = "flex";
 gameMode.style.justifyContent = "center";
 gameMode.style.alignItems = "center";
+gameMode.style.display = "none";
 
 board.style.width = BoardLenght + "px";
 board.style.height = BoardLenght + "px";
@@ -180,8 +198,40 @@ var boardArray = [
     Default, Default, Default
 ];
 
+function GetRandom(){
+    return Math.floor(Math.random() * 9);
+}
+
+function ComputerClick(){
+    Mode = AiMode;
+    chooseModeContainer.style.display = None;
+    chooseSymbolContainer.style.display = Block;
+}
+
+function MutiplayerClick(){
+    Mode = MutiMode;
+    chooseModeContainer.style.display = None;
+    chooseSymbolContainer.style.display = Block;
+}
+
+function XSymbolClick(){
+    UserOneSymbol = X;
+    UserTwoSymbol = O;
+    AiSymbol = O;
+    chooseMode.style.display = None;
+    gameMode.style.display = "flex";
+}
+
+function OSymbolClick(){
+    UserOneSymbol = O;
+    UserTwoSymbol = X;
+    AiSymbol = X;
+    chooseMode.style.display = None;
+    gameMode.style.display = "flex";
+}
+
 function UserClick(index){
-    if (boardArray[index] != UserOne && boardArray[index] != UserTwo){
+    if (Mode == MutiMode && boardArray[index] != UserOne && boardArray[index] != UserTwo){
         switch (index) {
             case 0:
                 if (WhoTurn == UserOne){
@@ -383,6 +433,13 @@ function UserClick(index){
                 break;
         }
     }
+    else if (Mode == AiMode && WhoTurn == UserOne &&
+        boardArray[index] != UserOne && boardArray[index] != AI)
+        {
+            boardArray[index] = UserOne;
+            DisplaySymbol();
+            WhoTurn = AI;
+        }
 }
 
 function DisplaySymbol(){
@@ -397,13 +454,17 @@ function DisplaySymbol(){
                         oOne.style.display = Block;
                     }
                 }
-                else if (boardArray[0] == UserTwo){
-                    if (UserTwoSymbol == X){
+                else if (boardArray[0] == AI){
+                    if (AiSymbol == X){
                         xOne.style.display = Block;
                     }
-                    else if (UserTwoSymbol == O){
+                    else if (AiSymbol == O){
                         oOne.style.display = Block;
                     }
+                }
+                else{
+                    xOne.style.display = None;
+                    oOne.style.display = None;
                 }
                 break;
             case 1:
@@ -415,29 +476,299 @@ function DisplaySymbol(){
                         oTwo.style.display = Block;
                     }
                 }
-                else if (boardArray[1] == UserTwo){
-                    if (UserTwoSymbol == X){
+                else if (boardArray[1] == AI){
+                    if (AiSymbol == X){
                         xTwo.style.display = Block;
                     }
-                    else if (UserTwoSymbol == O){
+                    else if (AiSymbol == O){
                         oTwo.style.display = Block;
                     }
                 }
+                else{
+                    xTwo.style.display = None;
+                    oTwo.style.display = None;
+                }
                 break;
             case 2:
+                if (boardArray[i] == UserOne){
+                    if (UserOneSymbol == X){
+                        xThree.style.display = Block;
+                    }
+                    else if (UserOneSymbol == O){
+                        oThree.style.display = Block;
+                    }
+                }
+                else if (boardArray[i] == AI){
+                    if (AiSymbol == X){
+                        xThree.style.display = Block;
+                    }
+                    else if (AiSymbol == O){
+                        oThree.style.display = Block;
+                    }
+                }
+                else{
+                    xThree.style.display = None;
+                    oThree.style.display = None;
+                }
                 break;
             case 3:
+                if (boardArray[i] == UserOne){
+                    if (UserOneSymbol == X){
+                        xFour.style.display = Block;
+                    }
+                    else if (UserOneSymbol == O){
+                        oFour.style.display = Block;
+                    }
+                }
+                else if (boardArray[i] == AI){
+                    if (AiSymbol == X){
+                        xFour.style.display = Block;
+                    }
+                    else if (AiSymbol == O){
+                        oFour.style.display = Block;
+                    }
+                }
+                else{
+                    xFour.style.display = None;
+                    oFour.style.display = None;
+                }
                 break;
             case 4:
+                if (boardArray[i] == UserOne){
+                    if (UserOneSymbol == X){
+                        xFive.style.display = Block;
+                    }
+                    else if (UserOneSymbol == O){
+                        oFive.style.display = Block;
+                    }
+                }
+                else if (boardArray[i] == AI){
+                    if (AiSymbol == X){
+                        xFive.style.display = Block;
+                    }
+                    else if (AiSymbol == O){
+                        oFive.style.display = Block;
+                    }
+                }
+                else{
+                    xFive.style.display = None;
+                    oFive.style.display = None;
+                }
                 break;
             case 5:
+                if (boardArray[i] == UserOne){
+                    if (UserOneSymbol == X){
+                        xSix.style.display = Block;
+                    }
+                    else if (UserOneSymbol == O){
+                        oSix.style.display = Block;
+                    }
+                }
+                else if (boardArray[i] == AI){
+                    if (AiSymbol == X){
+                        xSix.style.display = Block;
+                    }
+                    else if (AiSymbol == O){
+                        oSix.style.display = Block;
+                    }
+                }
+                else{
+                    xSix.style.display = None;
+                    oSix.style.display = None;
+                }
                 break;
             case 6:
+                if (boardArray[i] == UserOne){
+                    if (UserOneSymbol == X){
+                        xSelven.style.display = Block;
+                    }
+                    else if (UserOneSymbol == O){
+                        oSelven.style.display = Block;
+                    }
+                }
+                else if (boardArray[i] == AI){
+                    if (AiSymbol == X){
+                        xSelven.style.display = Block;
+                    }
+                    else if (AiSymbol == O){
+                        oSelven.style.display = Block;
+                    }
+                }
+                else{
+                    xSelven.style.display = None;
+                    oSelven.style.display = None;
+                }
                 break;
             case 7:
+                if (boardArray[i] == UserOne){
+                    if (UserOneSymbol == X){
+                        xEight.style.display = Block;
+                    }
+                    else if (UserOneSymbol == O){
+                        oEight.style.display = Block;
+                    }
+                }
+                else if (boardArray[i] == AI){
+                    if (AiSymbol == X){
+                        xEight.style.display = Block;
+                    }
+                    else if (AiSymbol == O){
+                        oEight.style.display = Block;
+                    }
+                }
+                else{
+                    xEight.style.display = None;
+                    oEight.style.display = None;
+                }
                 break;
             case 8:
+                if (boardArray[i] == UserOne){
+                    if (UserOneSymbol == X){
+                        xNine.style.display = Block;
+                    }
+                    else if (UserOneSymbol == O){
+                        oNine.style.display = Block;
+                    }
+                }
+                else if (boardArray[i] == AI){
+                    if (AiSymbol == X){
+                        xNine.style.display = Block;
+                    }
+                    else if (AiSymbol == O){
+                        oNine.style.display = Block;
+                    }
+                }
+                else{
+                    xNine.style.display = None;
+                    oNine.style.display = None;
+                }
                 break;
         }
     }
 }
+
+function IsWin(symbol){
+    if (
+		(boardArray[0] == symbol && boardArray[1] == symbol && boardArray[2] == symbol) ||
+		(boardArray[3] == symbol && boardArray[4] == symbol && boardArray[5] == symbol) ||
+		(boardArray[6] == symbol && boardArray[7] == symbol && boardArray[8] == symbol) ||
+
+		(boardArray[0] == symbol && boardArray[3] == symbol && boardArray[6] == symbol) ||
+		(boardArray[1] == symbol && boardArray[4] == symbol && boardArray[7] == symbol) ||
+		(boardArray[2] == symbol && boardArray[5] == symbol && boardArray[8] == symbol) ||
+
+		(boardArray[0] == symbol && boardArray[4] == symbol && boardArray[8] == symbol) ||
+		(boardArray[2] == symbol && boardArray[4] == symbol && boardArray[6] == symbol)
+		) {
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+function IsDraw(){
+    if (boardArray[0] == Default || boardArray[1] == Default || boardArray[2] == Default ||
+        boardArray[3] == Default || boardArray[4] == Default || boardArray[5] == Default ||
+		boardArray[6] == Default || boardArray[7] == Default || boardArray[8] == Default
+		) {
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+function sleep(ms){
+    const date = Date.now();
+    let current = null;
+    do{
+        current = Date.now();
+    }while(current - date < ms);
+}
+
+function showResult(message){
+    result.textContent = message;
+    resultContainer.style.display = "flex";
+}
+
+setInterval(()=>{
+    if (WhoTurn == AI){
+        var aiIndex = GetRandom();
+        if (boardArray[aiIndex] != UserOne && boardArray[aiIndex] != AI){
+            sleep(1000);
+            boardArray[aiIndex] = AI;
+            DisplaySymbol();
+            sleep(1000);
+            WhoTurn = UserOne;
+        }
+    }
+
+    // If draw
+    if (IsWin(UserOne)){
+        DisplaySymbol();
+        sleep(1500);
+        boardArray = [
+            Default, Default, Default,
+            Default, Default, Default,
+            Default, Default, Default
+        ];
+        DisplaySymbol();
+        playerScore++;
+    }
+    else if (IsWin(AI) || IsWin(UserTwo)){
+        DisplaySymbol();
+        sleep(1500);
+        boardArray = [
+            Default, Default, Default,
+            Default, Default, Default,
+            Default, Default, Default
+        ];
+        DisplaySymbol();
+        otherScore++;
+    }
+    else if (IsDraw()){
+        DisplaySymbol();
+        sleep(1500);
+        boardArray = [
+            Default, Default, Default,
+            Default, Default, Default,
+            Default, Default, Default
+        ];
+        DisplaySymbol();
+    }
+
+    // Show symbol on screen
+    if (UserOneSymbol == X){
+        playerSymbol.textContent = X;
+    }
+    else if (UserOneSymbol == O){
+        playerSymbol.textContent = O;
+    }
+
+    if (Mode == AiMode){
+        if (AiSymbol == X){
+            otherSymbol.textContent = X;
+        }
+        else if (AiSymbol == O){
+            otherSymbol.textContent = O;
+        }
+    }
+    else if(Mode == MutiMode){
+        if (UserTwoSymbol == X){
+            otherSymbol.textContent = X;
+        }
+        else if (UserTwoSymbol == O){
+            otherSymbol.textContent = O;
+        }
+    }
+    if (Mode == AiMode){
+        otherName.textContent = "COMPUTER";
+    }
+    else{
+        otherName.textContent = "OTHER";
+    }
+    score.textContent = playerScore + ' : ' + otherScore;
+});
